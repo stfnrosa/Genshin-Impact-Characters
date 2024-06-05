4907bab15481480eebff8a851776aa10aa60c95d
stfnrosa <rosaa.estefanii@gmail.com>
2024-06-05 17:14:45 -0300

    criando componente de banner

---
+++ charactersGenshin/src/componentes/Banner/Banner.jsx
@@ -0,0 +1,14 @@
+import './Banner.css'
+
+function Banner (){
+    return (
+        <div className='banner'>
+            <div className='conteudo'>
+            <h2>Personagens e Times</h2>
+            <p>organizados em um só lugar!</p>
+            </div>
+        </div>
+    )
+}
+
+export default Banner
